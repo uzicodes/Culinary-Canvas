d856737b3e3adc44f49a84cb6a531bad3a91b772
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-10-11 19:30:29 +0600

    added the search box in the navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,12 +40,20 @@ const Header = () => {
             </Link>
           </nav>
 
+
           {/* Right Side Actions */}
           <div className="flex items-center space-x-4">
-            {/* Search */}
-            <button className="p-2 text-gray-600 hover:text-primary-600">
-              <Search className="w-5 h-5" />
-            </button>
+            {/* Search Box */}
+            <form className="relative hidden md:block">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 text-sm bg-gray-100 text-black"
+              />
+              <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+                <Search className="w-5 h-5" />
+              </span>
+            </form>
 
             {/* Wishlist */}
             <button className="p-2 text-gray-600 hover:text-primary-600">
