cfd24cdeb1c039f75e26fbba7b1926f3aed53759
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-10-13 22:40:52 +0600

    "All Items" button in the navbar now always shows all items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
           <div className="flex items-center space-x-4">
             {/* Nav Links left of Search */}
             <nav className="hidden md:flex items-center space-x-8 relative">
-              <Link href="/all-items" className="text-gray-700 hover:text-primary-600 font-medium">
+              <Link href="/all-items" className="text-gray-700 hover:text-primary-600 font-medium" scroll={true}>
                 All Items
               </Link>
               <div className="relative">
