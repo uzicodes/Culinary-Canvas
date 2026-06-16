b25bb2f8cf63b6d5ffcd01262c53f95624be9b96
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-06-16 15:45:38 +0600

    chore: 'capacitor.js' package installed & config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.utsho.culinarycanvas',
+  appName: 'Culinary Canvas',
+  webDir: 'public'
+};
+
+export default config;
