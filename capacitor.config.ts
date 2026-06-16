d0eb9c143fdf3688d3d3fe3b00ce7a092bd5b1c1
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-06-16 15:50:54 +0600

    chore: andriod package installation in /andriod root directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.utsho.culinarycanvas',
   appName: 'Culinary Canvas',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    url: 'https://the-culinary-canvas.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
